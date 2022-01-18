3ac63ad0c851287360ec25df814057ab10d58a33
mojomojo1 <mojomojo1@users.noreply.github.com>
2022-01-18 20:54:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, from inside the original application, with changes!"
+    return "Hello World, from inside the original application, with changes in a new branch!"
 
 
 if __name__ == "__main__":
